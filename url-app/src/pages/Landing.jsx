e8bccb146f0fc01ecc46bcb3a42957a641ef06ee
manoj <115392548+manoj-m001@users.noreply.github.com>
2026-02-20 22:26:32 +0530

    Remove unused faLink import from Landing.jsx

--- url-app/src/pages/Landing.jsx
+++ url-app/src/pages/Landing.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
 export default function Landing() {
   return (
     <div className="landing">
