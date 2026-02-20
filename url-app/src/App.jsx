f7853a694f1366b9f38766d9c92177fd8eed7797
manoj-netizenn <mallappanavarmanoj@gmail.com>
2026-02-20 20:19:18 +0530

    Initial commit

--- url-app/src/App.jsx
+++ url-app/src/App.jsx
@@ -1,19 +1,33 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Landing from './pages/Landing';
-import StatsPage from './pages/StatsPage';
-import RedirectPage from './pages/RedirectPage';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import StatsPage from "./pages/StatsPage";
+import RedirectPage from "./pages/RedirectPage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
         <header className="app-header">
           <div className="header-inner">
-            <Link to="/" className="brand">TinyLink</Link>
+            <Link to="/" className="brand">
+              TinyLink
+            </Link>
             <nav>
-              <Link to="/" className="nav-link">Home</Link>
-              <Link to="/dashboard" className="nav-link">Dashboard</Link>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+              <Link to="/dashboard" className="nav-link">
+                Dashboard
+              </Link>
+              <Link
+                to="https://github.com/manoj-m001/"
+                className="nav-link"
+              >
+                 <FontAwesomeIcon icon={faGithub} size="lg" />
+              </Link>
             </nav>
           </div>
         </header>
